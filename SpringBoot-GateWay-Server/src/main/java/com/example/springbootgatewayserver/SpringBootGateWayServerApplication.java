d0d631f1e387d3d3f468c1621d83f2e46d17cc59
zhongxiang <550037499@qq.com>
2022-09-08 20:34:29 +0800

    修改了父项目的pom文件

--- SpringBoot-GateWay-Server/src/main/java/com/example/springbootgatewayserver/SpringBootGateWayServerApplication.java
+++ SpringBoot-GateWay-Server/src/main/java/com/example/springbootgatewayserver/SpringBootGateWayServerApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.netflix.zuul.filters.discovery.PatternServiceRouteMapper;
+//import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+//import org.springframework.cloud.netflix.zuul.filters.discovery.PatternServiceRouteMapper;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
